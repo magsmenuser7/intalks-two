804d1f113c5a0565c78e97b43724d7eff1353ab3
suresh522 <kajasuresh522@gmail.com>
2024-10-30 16:43:01 +0530

    changes done

--- assets/js/plugins/contact.form.js
+++ assets/js/plugins/contact.form.js
@@ -52,7 +52,7 @@
                 if (data.responseText !== '') {
                     $(formMessages).text(data.responseText);
                 } else {
-                    // $(formMessages).text('Oops! An error occured and your message could not be sent.');
+                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
                 }
             });
     });
